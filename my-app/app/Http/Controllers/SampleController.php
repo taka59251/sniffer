fbed6e3beea5263b12ce8e0a2edbf90118c62802
yamada takanori <yamada@entermedia.co.jp>
2022-08-29 17:59:11 +0900

    code check

---
+++ my-app/app/Http/Controllers/SampleController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SampleController extends Controller
+{
+public function test()
+{
+$a = [
+    '1' => 'ok',
+    '2' => 'ng'
+];
+
+$b = array('ok', 'ng');
+
+print_r($a);
+print_r($b);
+}
+}
