d3a1ce30cdbc8139c3239adf06b12d84fbefd9e4
yamada-t@mcury <yamada-t@mcury.jp>
2022-08-30 14:49:11 +0900

    no message

--- my-app/app/Http/Controllers/SampleController.php
+++ my-app/app/Http/Controllers/SampleController.php
@@ -6,9 +6,8 @@ use Illuminate\Http\Request;
 
 class SampleController extends Controller
 {
-    public function test()
-    {
-        $a = [
+    public function test(){
+        $a=[
             '1' => 'ok',
             '2' => 'ng'
         ];
