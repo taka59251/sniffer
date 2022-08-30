4f91012548e4d29cbd9bc9dc84e78932cebeba17
yamada-t@mcury <yamada-t@mcury.jp>
2022-08-30 10:08:54 +0900

    no message

--- my-app/app/Http/Controllers/SampleController.php
+++ my-app/app/Http/Controllers/SampleController.php
@@ -6,16 +6,16 @@ use Illuminate\Http\Request;
 
 class SampleController extends Controller
 {
-    public function test()
-    {
-        $a = [
-            '1' => 'ok',
-            '2' => 'ng'
-        ];
+public function test()
+{
+$a = [
+    '1' => 'ok',
+    '2' => 'ng'
+];
 
-        $b = array('ok', 'ng');
+$b = array('ok', 'ng');
 
-        print_r($a);
-        print_r($b);
-    }
+print_r($a);
+print_r($b);
+}
 }
